4a64872283f7099e173c977696b2016010923582
Eduardo Alvarez <eduardo.alvarez@globant.com>
2019-03-25 18:01:57 -0300

    Inicio y termino del proyecto xD

---
+++ build/js/main.js
@@ -0,0 +1,12 @@
+(function(){
+  setTimeout(function(){
+    const screen = document.querySelector(".bmo__head__screen");
+    const img = document.createElement("img");
+
+    img.src = "./build/img/adventure_time.gif";
+    img.setAttribute('class', 'bmo__head__screen__gif');
+
+    screen.innerHTML = "";
+    screen.appendChild(img);
+  }, 5000);
+})();
